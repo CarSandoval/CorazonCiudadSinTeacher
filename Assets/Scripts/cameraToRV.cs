e195289f63c790b86119f8f2d1c437170648e0bd
Carlos Sandoval <chcarlossandoval@gmail.com>
2019-06-29 07:18:00 -0500

    arhivos nuevos que si jalan pero sin VR

--- Assets/Scripts/cameraToRV.cs
+++ Assets/Scripts/cameraToRV.cs
@@ -3,17 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.VR;
 
 public class cameraToRV : MonoBehaviour
 {
 
-    private IEnumerator coroutine;
+    //private IEnumerator coroutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        coroutine = SwitchToVR();
-        StartCoroutine(coroutine);
+        StartCoroutine(SwitchToVR());
+        //Camera.main.ResetAspect();
     }
 
     // Update is called once per frame
@@ -22,22 +23,19 @@ public class cameraToRV : MonoBehaviour
         
     }
 
-    IEnumerator SwitchToVR()
-    {
-        // Device names are lowercase, as returned by `XRSettings.supportedDevices`.
-        string desiredDevice = "daydream"; // Or "cardboard".
 
-        // Some VR Devices do not support reloading when already active, see
-        // https://docs.unity3d.com/ScriptReference/XR.XRSettings.LoadDeviceByName.html
-        if (String.Compare(XRSettings.loadedDeviceName, desiredDevice, true) != 0)
+    private IEnumerator SwitchToVR()
+    {
+        string desiredDevice = "cardboard";
+        //Camera.main.transform.localRotation = Quaternion.identity;
+        if (string.Compare(XRSettings.loadedDeviceName, desiredDevice, true) != 0)
         {
             XRSettings.LoadDeviceByName(desiredDevice);
-
-            // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
             yield return null;
         }
-
-        // Now it's ok to enable VR mode.
         XRSettings.enabled = true;
     }
+
+
+
 }
