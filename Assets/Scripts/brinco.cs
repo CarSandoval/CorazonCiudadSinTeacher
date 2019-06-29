549bdb98a2579e0d3d74761dc238275647be1747
Carlos Sandoval <chcarlossandoval@gmail.com>
2019-06-29 03:05:35 -0500

    actualizacion de tagets, y url de los videos

--- Assets/Scripts/brinco.cs
+++ Assets/Scripts/brinco.cs
@@ -1,29 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class brinco : MonoBehaviour
+﻿namespace Mapbox.Examples
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Mapbox.Unity.Location;
+    using Mapbox.Unity.Map;
 
-    public float velocidad=1;
-    public float amplitud=1;
-
-    int cont = 0;
-    // Start is called before the first frame update
-    void Start()
+    public class brinco : MonoBehaviour
     {
-        cont = 0;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public float velocidad = 1;
+        public float amplitud = 1;
+        public float latitud;
+        public float longitud;
+        int cont = 0;
 
-    private void FixedUpdate()
-    {
-        transform.transform.Translate(0,amplitud*Mathf.Sin((float)cont*velocidad),0);
-        cont++;
+        bool _isInitialized;
+
+        ILocationProvider _locationProvider;
+        ILocationProvider LocationProvider
+        {
+            get
+            {
+                if (_locationProvider == null)
+                {
+                    _locationProvider = LocationProviderFactory.Instance.DefaultLocationProvider;
+                }
+
+                return _locationProvider;
+            }
+        }
+        // Start is called before the first frame update
+
+        void Start()
+        {
+            LocationProviderFactory.Instance.mapManager.OnInitialized += () => _isInitialized = true;
+            cont = (int)Random.Range(0,360);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {   
+            
+        }
+
+        void LateUpdate()
+        {
+            if (_isInitialized)
+            {
+                var map = LocationProviderFactory.Instance.mapManager;
+                transform.localPosition = map.GeoToWorldPosition(new Utils.Vector2d(latitud, longitud));
+                transform.transform.Translate(0, amplitud * Mathf.Sin((float)cont * velocidad), 0);
+                cont++;
+                //transform.localPosition = map.GeoToWorldPosition(new Utils.Vector2d(60.19188, 24.9685822));
+                //transform.localPosition = map.GeoToWorldPosition(new Utils.Vector2d(20.6799374, -103.3496198));
+            }
+        }
     }
 }
--- Assets/Scripts/tocarObjeto.cs
+++ Assets/Scripts/tocarObjeto.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class tocarObjeto : MonoBehaviour
 {
+    public GameObject personaje;
+    public float distancia = 15f;
+    public string url;
     // Use this for initialization
     void Start()
     {
@@ -21,9 +24,10 @@ public class tocarObjeto : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 //Select Stage
-                if (hit.transform.name == transform.name)
+                if (hit.transform.name == transform.name && Vector3.Distance(transform.position, personaje.transform.position)<distancia)
                 {
                     //<SceneManager.LoadScene("SceneTwo");
+                    Application.OpenURL(url);
                     print("toque!!!");
                 }
             }
